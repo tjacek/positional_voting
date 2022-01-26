e7b014b5cbb84b5ee1e622be2a878429d151712f
tjacek <tjacek@agh.edu.pl>
2022-01-27 00:42:47 +0100

    acc exp

--- exp.py
+++ exp.py
@@ -1,3 +1,5 @@
+import os
+import numpy as np
 from functools import wraps
 import files,ens
 
@@ -15,7 +17,8 @@ def dir_function(args=None):
                     out_i=f"{args[1]}/{name_i}"
                     new_args=(path_i,out_i)	
                 else:
-                	new_args=(path_i)
+                	new_args=(path_i,)
+                print(new_args)
                 output.append( fun(*new_args,**kwargs))
             return output
         return dir_decorator
@@ -27,3 +30,16 @@ def ens_results(in_path,out_path=None,clf="LR"):
     paths=(f"{in_path}/common",f"{in_path}/binary")
     votes=ens.make_votes(paths,clf=clf)
     votes.save(out_path)
+
+@dir_function(args=None)
+def acc_exp(in_path):
+    results=[ens.read_votes(path_i).voting()
+        for path_i in files.top_files(in_path)]
+    acc=[result_i.get_acc() for result_i in results]
+    mean_acc,std_acc=np.mean(acc),np.std(acc)
+    name_i=in_path.split("/")[-1] 
+    return (name_i,mean_acc,std_acc)
+
+if __name__ == "__main__":
+    acc=acc_exp("B/results")
+    print(acc)   
