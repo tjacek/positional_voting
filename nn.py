f5517759519d20542305640826f534c8659f03e8
tjacek <tjacek@agh.edu.pl>
2022-01-30 00:05:53 +0100

    nn module

--- nn.py
+++ nn.py
@@ -7,8 +7,7 @@ from tensorflow.keras.layers import Dense,BatchNormalization
 from tensorflow.keras import regularizers
 from tensorflow.keras import optimizers
 from tensorflow.keras import Input, Model
-#import random
-import exp,feats#convert,files,feats,learn
+import exp,feats,files,learn
 
 class SimpleNN(object):
     def __init__(self,n_hidden=10):
@@ -55,30 +54,18 @@ def simple_ensemble(dataset,out_path,
         feats_i.save(out_i)
     return accuracy
 
-#@files.dir_function(recreate=False)
-#def make_datasets(in_path,out_path,n_epochs=150):
-#    print(in_path)
-#    print(out_path)
-#    simple_ensemble(dataset,out_path,n_epochs=150)
-
 @exp.dir_function()
 @exp.multi_iter(n_iters=10,in_iter=False)
-def random_ensemble(in_path,out_path):
+def random_ensemble(in_path,out_path,n_epochs=150):
     print(in_path)
     print(out_path)
+    files.make_dir(out_path)
     data_i=feats.read(in_path)[0]
     random_data=data_i.balanced_split()
-    print(random_data.n_cats())
-
-#    files.make_dir(out_path)
-#    for i in range(n_iters):
-#        out_i=f"{out_path}/{i}"
-#        files.make_dir(out_i)
-#        data_i=feats.read(in_path)[0]
-#        random_data= data_i.random()
-#        random_data.save(f"{out_i}/common")
-#        simple_ensemble(random_data,f"{out_i}/binary",
-#            n_hidden=30,batch_size=1,n_epochs=n_epochs)
+    random_data.save(f"{out_path}/common")
+    n_hidden=min( (30,random_data.dim()[0]))   
+    simple_ensemble(random_data,f"{out_path}/binary",
+            n_hidden=n_hidden,batch_size=1,n_epochs=n_epochs)
 
 if __name__ == "__main__":
 #    dataset=convert.txt_dataset("penglung/raw.data")
