8b9ae76599c242a5188fa002a79a72efaaaf5572
tjacek <tjacek@agh.edu.pl>
2022-02-04 02:09:03 +0100

    GridOptim

--- cv.py
+++ cv.py
@@ -12,8 +12,6 @@ class ClfCV(object):
     @exp.dir_function(clf_decor=True)
 #    @exp.if_exist
     def __call__(self,in_path,out_path):
-        print(in_path)
-        print(out_path)
         in_path=f"{in_path}/common"
         data_i=feats.read(in_path)[0]
         print(len(data_i))
@@ -25,37 +23,37 @@ class ClfCV(object):
         votes_i=self.get_votes(full_tuple,clf_i)
         votes_i.save(out_path)
 
-def gs_rf(X_train,y_train):
+class GridOptim(object):
+    def __init__(self,clf,params):
+        self.clf=clf
+        self.params=params
+
+    def __call__(self,X_train,y_train):
+        clf_i = GridSearchCV(self.clf(),self.params,
+                    verbose=1,
+                    scoring='neg_log_loss')
+        clf_i.fit(X_train,y_train)    
+        best_params=clf_i.cv_results_['params'][0]
+        return clf_i.best_estimator_,best_params
+
+def gs_rf():
     params={'max_depth': [3, 5, 10],
             'min_samples_split': [2, 5, 10]}
-    clf_i = ensemble.RandomForestClassifier()#max_depth=3)
-    clf_i = GridSearchCV(clf_i,params,
-                    verbose=1,
-                    scoring='neg_log_loss')
-    clf_i.fit(X_train,y_train)    
-    best_params=clf_i.cv_results_['params'][0]
-    return clf_i.best_estimator_,best_params
+    clf = ensemble.RandomForestClassifier
+    grid=GridOptim(clf,params)
+    return ClfCV(grid,get_votes)
 
-def gs_bag(X_train,y_train):
-    params={'n_estimators': [5,10,15,20],
-            }
-    clf_i = ensemble.BaggingClassifier()#max_depth=3)
-    clf_i = GridSearchCV(clf_i,params,
-                    verbose=1,
-                    scoring='neg_log_loss')
-    clf_i.fit(X_train,y_train)    
-    best_params=clf_i.cv_results_['params'][0]
-    return clf_i.best_estimator_,best_params
+def gs_bag():
+    params={'n_estimators': [5,10,15,20]}
+    clf = ensemble.BaggingClassifier
+    grid=GridOptim(clf,params)
+    return ClfCV(grid,get_votes)
 
-def gs_boost(X_train,y_train):
-    clf_i = ensemble.GradientBoostingClassifier()#max_depth=3)
-    clf_i = GridSearchCV(clf_i,{'max_depth': [2,4,6],
-                    'n_estimators': [5,10,15,20]},
-                    verbose=1,
-                    scoring='neg_log_loss')
-    clf_i.fit(X_train,y_train)    
-    best_params=clf_i.cv_results_['params'][0]
-    return clf_i.best_estimator_,best_params
+def gs_boost():
+    params={'max_depth': [2,4,6],'n_estimators': [5,10,15,20]}
+    clf = ensemble.GradientBoostingClassifier
+    grid=GridOptim(clf,params)
+    return ClfCV(grid,get_boost_votes)
 
 def get_boost_votes(result_tuple,clf_i):
     X,y_true,names=result_tuple
@@ -68,7 +66,7 @@ def get_boost_votes(result_tuple,clf_i):
         else:
             y_pred= (y_raw<0).astype(int)
             y_pred= y_pred.ravel()
-        result_j=learn.make_result(y_pred,names) #learn.Result(y_true,y_pred,names)
+        result_j=learn.make_result(y_pred,names)
         results.append(result_j)
     return ens.Votes(results)
 
@@ -83,9 +81,9 @@ def get_votes(result_tuple,clf_i):
 
 def clf_exp(in_path,out_path):
     files.make_dir(out_path)
-    algs={"RF":ClfCV(gs_rf,get_votes),
-          "BAG":ClfCV(gs_bag,get_votes),
-          "BOOST":ClfCV(gs_boost,get_boost_votes)}
+    algs={"RF":gs_rf(),
+          "BAG":gs_bag(),
+          "BOOST":gs_boost()}
     for name_i,alg_i in algs.items():
         out_i=f"{out_path}/{name_i}"
         alg_i(in_path,out_i)
