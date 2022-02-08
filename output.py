dcfccda26f1401ede587412c41fc2622ae5269fb
tjacek <tjacek@student.agh.edu.pl>
2022-02-08 22:49:39 +0100

    by_voting

--- output.py
+++ output.py
@@ -1,6 +1,15 @@
 import pandas
 
-data=pandas.read_csv("auc.csv")
+def by_voting(paths):
+    dataset=[pandas.read_csv(path_i)
+                for path_i in paths]
+    vote_dicts={}
+    key_names=['dataset','clf']
+    for data_i in dataset:
+        for vote_j in data_i.voting.unique():
+            data_ij=data_i[data_i['voting']==vote_j]	
+            vote_dicts[vote_j]=to_dict(data_ij,key_names)
+    return vote_dicts
 
 def to_dict(data,key_names):
     data = data.reset_index()  
@@ -11,5 +20,7 @@ def to_dict(data,key_names):
         data_dict[key_i]=row
     return data_dict
 
-data_dict=to_dict(data,['dataset','clf'])
-print(data_dict.keys())
+vote_dicts=by_voting(['bayes.csv','auc.csv'])
+print(vote_dicts.keys())
+#data_dict=to_dict(data,)
+#print(data_dict.keys())
