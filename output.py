ad81cedb3fcc4374391cc50d78072489a213dae4
tjacek <tjacek@student.agh.edu.pl>
2022-02-13 17:17:16 +0100

    order keys

--- output.py
+++ output.py
@@ -5,10 +5,10 @@ def by_voting(paths):
     dataset=[pandas.read_csv(path_i)
                 for path_i in paths]
     vote_dicts={}
-    key_names=['dataset','clf']
+    key_names=['Dataset','Clf']
     for data_i in dataset:
         for vote_j in data_i.voting.unique():
-            data_ij=data_i[data_i['voting']==vote_j]	
+            data_ij=data_i[data_i['Voting']==vote_j]	
             vote_dicts[vote_j]=to_dict(data_ij,key_names)
     return vote_dicts
 
@@ -34,9 +34,9 @@ def compare_output(pair,attr,vote_dicts):
 
 def best_attr(in_path,attr='auc_mean'):
     exp_output = pandas.read_csv(in_path)
-    show_attr=['dataset','clf','voting']
-    for name_i in exp_output.dataset.unique():
-        sub=exp_output[exp_output.dataset==name_i]
+    show_attr=['Dataset','Clf','Voting']
+    for name_i in exp_output.Dataset.unique():
+        sub=exp_output[exp_output.Dataset==name_i]
         index=sub[attr].idxmax()
         row_i= exp_output.iloc[[index]]
         desc_i=[row_i[attr_i].values[0]  
@@ -52,24 +52,34 @@ def to_doc(in_path,out_path):
 
     document = Document()
     dataset_dict={ name_i:[]
-       for name_i in exp_output.dataset.unique()}
+       for name_i in exp_output.Dataset.unique()}
     for i, row_i in exp_output.iterrows():
-        dataset_dict[row_i['dataset']].append(row_i)
+        dataset_dict[row_i['Dataset']].append(row_i)
     for name_i,lines_i in dataset_dict.items():
         table_i=document.add_table(rows=len(lines_i)+1,
             cols=len(col_names))        
         fill_rows(table_i,0,col_names)
-        for j,line_j in enumerate(lines_i):
+        
+        line_dict={get_key(line_j):line_j 
+            for line_j in lines_i}
+        keys=list(line_dict.keys())
+        keys.sort()
+        for j,key_j in enumerate(keys):
+            line_j=line_dict[key_j]
             fill_rows(table_i,j+1,line_j)
         document.add_paragraph()
     document.save(out_path)
+
+def get_key(line_j):
+    return "_".join([line_j['Clf'],line_j['Voting']])
 
 def fill_rows(table_i,j,values):
     cells = table_i.rows[j].cells
     for i,value_i in enumerate(values):
         cells[i].text=str(value_i)    
 
-#best_attr('bayes.csv',attr='auc_mean')
-vote_dicts=by_voting(['one_vs_all.csv'])
-print(vote_dicts.keys())
-compare_output(['raw','opv_acc'],'auc_mean',vote_dicts)
+best_attr('full.csv',attr='Acc_mean')
+#vote_dicts=by_voting(['full.csv'])
+#print(vote_dicts.keys())
+#compare_output(['raw','opv_auc'],'auc_mean',vote_dicts)
+#to_doc('full.csv','full.doc')
