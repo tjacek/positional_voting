10d209918272ee7382bb2171e0bb4d5a3c94187a
tjacek <tjacek@student.agh.edu.pl>
2022-06-15 01:43:09 +0200

    compute inliners

--- inliner.py
+++ inliner.py
@@ -1,4 +1,6 @@
-import format,exp,ens
+import numpy as np
+from sklearn import neighbors
+import format,exp,ens,learn
 
 @exp.dir_function(clf_decor=False)
 @exp.dir_function(clf_decor=False)
@@ -6,7 +8,18 @@ def fun(in_path):
     paths=(f"{in_path}/common",f"{in_path}/binary")
     votes=ens.make_votes(paths,clf="LR")
     binary_feats=ens.read_dataset((0,paths[1]))
+    inliners=[get_knn(feats_i)
+                 for feats_i in binary_feats]
     print(len(binary_feats))
+
+
+def get_knn(data_i,k=3):
+    clf= neighbors.KNeighborsClassifier(k)
+    result_i=learn.train_model(data_i,clf_type=clf)
+    test_result= result_i.split()[1]     
+    y_true,y_pred,names=test_result.get_pred()
+    inliner_dict=dict(zip(names,zip(y_true,y_pred)))  
+    return inliner_dict
 
 in_path= "data"
 fun(in_path)
