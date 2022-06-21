e2163c4831a46a76b73cf1574a96392182fd6df1
tjacek <tjacek@student.agh.edu.pl>
2022-06-21 23:44:16 +0200

    attr_corel

--- sql.py
+++ sql.py
@@ -12,14 +12,14 @@ def as_latex(in_path,cols):
         else:
             new_cols[col_i]=df[col_i]
     final = pd.DataFrame(new_cols)
-    print(final.to_latex())
+    print(final.to_csv())
 
-def by_voting(in_path):
-    df=pd.read_csv(in_path)
-    voting_type=['borda','opv_acc','opv_auc','opv_f1','raw']
-    df_dict={vote_i:df[df['Voting']==vote_i]
-               for vote_i in voting_type}
-    print(df_dict['borda']['Dataset'].unique())
+#def by_voting(in_path):
+#    df=pd.read_csv(in_path)
+#    voting_type=['borda','opv_acc','opv_auc','opv_f1','raw']
+#    df_dict={vote_i:df[df['Voting']==vote_i]
+#               for vote_i in voting_type}
+#    print(df_dict['borda']['Dataset'].unique())
 
 def dataset_stats(in_path):
     paths=format.find_result(in_path,"common")
@@ -33,10 +33,22 @@ def dataset_stats(in_path):
     cols=['Dataset','Samples','Feats','Cats' ]
     return pd.DataFrame(raw,columns=cols)
 
-cols=["Dataset","sucess","neutral","fail"]
+def attr_corel(paths,pairs):
+    frame=[pd.read_csv(path_i) for path_i in paths]
+    final=frame[0].merge(frame[1],left_on='Dataset', right_on='Dataset')
+    main_attr,attrs=pairs
+    main_ts=final[main_attr]
+    raw=[]
+    for attr_i in attrs:
+        corl_i=main_ts.corr(final[attr_i])
+        raw.append([main_attr,attr_i, corl_i ])
+    cols=['Attr X','Attr Y','Corl' ]
+    return pd.DataFrame(raw,columns=cols)
 
-#"old_mean","old_std","old_max","new_mean",,"new_max"]
-#as_latex("knn/succ.csv",cols)
-#by_voting('full.csv')
-df=dataset_stats("data")
-print(df.to_csv())
+#cols=["Dataset","sucess","neutral","fail"]
+#cols=["Dataset",("new_mean","old_mean")]
+#as_latex("knn/stats.csv",cols)
+#df=dataset_stats("data")
+pairs=("diff_new_mean",["Samples","Feats","Cats"])
+df=attr_corel(["knn/datasets.csv","knn/diff.csv"],pairs)
+print(df)
