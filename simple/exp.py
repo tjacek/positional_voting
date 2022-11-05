d8af80b42aadff95109db69103d0d90d1242b20e
tjacek <tjacek@student.agh.edu.pl>
2022-11-05 01:56:04 +0100

    cv_exp

---
+++ simple/exp.py
@@ -0,0 +1,15 @@
+import protocols,utils,clfs,cv
+
+def cv_exp(in_path="cleveland",out_path='balanced',pairs=None,
+	    clf_alg=None,n_iters=10):
+    if(pairs is None):
+        pairs=[(0,2),(0,3),(0,4)]
+    if(clf_alg is None):
+        clf_alg=clfs.rf_clf()
+    utils.make_dir(out_path)
+    for pair_i in pairs:
+        selector=cv.BalancedSelector(*pair_i)
+        opv_exp=protocols.OPVExp(limit=12,selector=selector)
+        output=protocols.multi_exp(in_path,clf_alg,metric=None,
+            n_iters=n_iters,opv_exp=opv_exp)
+        output.save(f'{out_path}/{pair_i[1]}')
