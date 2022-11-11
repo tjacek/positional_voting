e0b94fa48bda150ca75a7a6bbc353bcf4109487f
tjacek <tjacek@student.agh.edu.pl>
2022-11-11 23:56:34 +0100

    diff format

--- simple/format.py
+++ simple/format.py
@@ -6,9 +6,9 @@ class OutputDict(dict):
     
     def show(self,fun=None):
         if(fun is None):
-            fun=lambda out_i:out_i.diff(True)
+            fun=lambda out_i:f"{out_i.diff(True):2.4f}"
         for name_i,out_i in self.items():
-            print(f"{name_i},{fun(out_i):2.4f}")
+            print(f"{name_i},{fun(out_i)}")
 
 def format_output(in_path):
     all_outputs={}	
@@ -17,7 +17,13 @@ def format_output(in_path):
         all_outputs[path_i.split('/')[-1]]=out_i
     return OutputDict(all_outputs)
 
+def all_diff(output):
+    def helper(out_i):
+        lines=[ "%.4f-%.4f" % (base_i,opv_i)
+            for base_i,opv_i in zip(*out_i.get_acc())]
+        return ",".join(lines)
+    return output.show(helper)	 
 #def line(name_i):
 
 d=format_output("cmc")
-d.show()
+all_diff(d)
