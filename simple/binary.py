fb38a6cbd8a651d904b650a0c65f4ff835b09c0e
tjacek <tjacek@agh.edu.pl>
2022-11-27 20:42:53 +0100

    binary class

--- simple/binary.py
+++ simple/binary.py
@@ -1,7 +1,18 @@
+import protocols,data,clfs
 
 class SimpleBinary(object):
-    def __init__(self,n_hidden):
+    def __init__(self,n_hidden=25):
         self.n_hidden=n_hidden
         	
     def fit(self,data_i):
         return None
+
+def binary_clf():
+    clf = SimpleBinary   
+    params={'n_hidden': [25,50,100]}
+    return clfs.ClfAlg(clf,params,"binary")
+
+d=data.read_data("cleveland.json")
+train=d.split()[0]
+clf_alg=binary_clf()
+protocols.find_hyperparams(train,clf_alg)
