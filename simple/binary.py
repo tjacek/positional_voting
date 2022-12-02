1c3ec1371c344440c605df4227df6a724c6b7cfb
tjacek <tjacek@agh.edu.pl>
2022-12-02 23:57:20 +0100

    binary

--- simple/binary.py
+++ simple/binary.py
@@ -1,18 +1,28 @@
+import tensorflow.keras
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,BatchNormalization
+from tensorflow.keras import regularizers
 from tensorflow.keras import optimizers
 from tensorflow.keras import Input, Model
 from sklearn.base import BaseEstimator, ClassifierMixin
 import protocols,data,clfs
 
-class SimpleBinary(BaseEstimator, ClassifierMixin):
+class BinaryEnsemble(BaseEstimator, ClassifierMixin):
     def __init__(self,n_hidden=25):
         self.n_hidden=n_hidden
-        	
+        self.models=[]
+
     def fit(self,data_i,targets):
         params={'dims':data_i.shape[0],'n_cats':data_i.shape[1]}
-        raise Exception(params)
-        models,accuracy=[],[]
+#        raise Exception(params)
+#        accuracy=[],[]
         for cat_i in range(params['n_cats']):
-            model_i=SimpleNN(n_hidden=n_hidden)(params)
+            model_i=SimpleNN(n_hidden=self.n_hidden)(params)
+            self.models.append(model_i)
+        return None
+
+    def predict(self):
         return None
 
 
@@ -32,11 +42,11 @@ class SimpleNN(object):
         return model
 
 def binary_clf():
-    clf = SimpleBinary   
+    clf = BinaryEnsemble   
     params={'n_hidden': [25,50,100]}
     return clfs.ClfAlg(clf,params,"binary")
 
-d=data.read_data("datasets/cmc.json")
+d=data.read_data("cleveland.json")
 train=d.split()[0]
 clf_alg=binary_clf()
 protocols.find_hyperparams(train,clf_alg)
