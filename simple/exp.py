090d854a9c68705d109b7d915efdf7d7c9f0fee2
tjacek <tjacek@student.agh.edu.pl>
2022-12-19 23:41:29 +0100

    multi_exp

--- simple/exp.py
+++ simple/exp.py
@@ -20,14 +20,21 @@ def inspect(in_path):
         (base_i,opv_i),diff_i=out_i.mean(),out_i.diff(True)
         print(f"{path_i}base:{base_i:2.2f},opv:{opv_i:2.2f},diff{diff_i:2.4f}")
 
+def multi_exp(in_path,out_path,names,protocols,n_iters=10):
+    for name_i in names:
+        in_i,out_i=f"{in_path}/{name_i}",f"{out_path}/{name_i}"
+        protocol(in_i,out_i,n_iters)
+
 def make_protocol():
     selector=cv.BalancedSelector(0,4)
     opv_exp=protocols.OPVExp(limit=12,selector=selector)
     clf_algs=[clfs.rf_clf(),clfs.bag_clf()]
     metrics=[opv.auc_metric,opv.acc_metric,opv.f1_metric]
-    return protocols.Protocol(clf_algs,metrics,opv_exp=opv_exp)
+    prot= protocols.Protocol(clf_algs,metrics,opv_exp=opv_exp)
+    return prot#utils.dir_exp(prot)
 
 protocol= make_protocol()
-protocol("datasets/cmc.json",'cmc',n_iters=10)
+#protocol("datasetsII",'raw',n_iters=10)
+protocol("datasetsII/satimage.json",'full/satimage',n_iters=10)
 print(protocol.exp_log)
 #inspect("full")
