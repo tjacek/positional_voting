cbbe7eb430550541952690c3288a918216589347
tjacek <tjacek@agh.edu.pl>
2022-12-20 23:52:45 +0100

    nn

---
+++ ECSCF/nn.py
@@ -0,0 +1,21 @@
+import tensorflow.keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,BatchNormalization
+from tensorflow.keras import regularizers
+from tensorflow.keras import optimizers
+from tensorflow.keras import Input, Model
+
+class SimpleNN(object):
+    def __init__(self,n_hidden=10):
+        self.n_hidden=n_hidden
+        self.optim=optimizers.RMSprop(learning_rate=0.00001)
+
+    def __call__(self,params):
+        model = Sequential()
+        model.add(Dense(self.n_hidden, input_dim=params['dims'], activation='relu',name="hidden",
+            kernel_regularizer=regularizers.l1(0.001)))
+        model.add(BatchNormalization())
+        model.add(Dense(params['n_cats'], activation='softmax'))
+        model.compile(loss='categorical_crossentropy',optimizer=self.optim, metrics=['accuracy'])
+        model.summary()
+        return model
