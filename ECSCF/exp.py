60dc11d7710d402737c6b0703331d9d4b9d34f72
tjacek <tjacek@agh.edu.pl>
2022-12-21 09:36:10 +0100

    compare_clfs

--- ECSCF/exp.py
+++ ECSCF/exp.py
@@ -1,12 +1,24 @@
 from sklearn.metrics import classification_report,accuracy_score,f1_score
+from sklearn import ensemble
+
 import data,ecscf
 
-d=data.read_data('wine.json')
+def simple_exp(d,clf_alg):
+    train,test=d.split()
+    X,y,names=train.as_dataset()
+    clf_alg.fit(X,y)
+    X,y_true,names=test.as_dataset()
+    y_pred=clf_alg.predict(X)
+    print(f'{clf_alg}:{accuracy_score(y_true,y_pred)}')
 
-clf_alg=ecscf.ECSCF()
-train,test=d.split()
-X,y,names=train.as_dataset()
-clf_alg.fit(X,y)
-X,y_true,names=test.as_dataset()
-y_pred=clf_alg.predict(X)
-print(accuracy_score(y_true,y_pred))
+def compare_clfs(d):
+    clf_alg=ecscf.ECSCF()
+    simple_exp(d,clf_alg)
+    clf_alg = ensemble.RandomForestClassifier()
+    simple_exp(d,clf_alg)
+    clf_alg = ensemble.BaggingClassifier()
+    simple_exp(d,clf_alg)
+
+d=data.read_data('wine.json')
+compare_clfs(d)
+
