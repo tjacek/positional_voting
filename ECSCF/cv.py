afefbdfcd0bbddf6738b89e354af897ad475d2cd
tjacek <tjacek@agh.edu.pl>
2022-12-27 17:43:59 +0100

    read folds

--- ECSCF/cv.py
+++ ECSCF/cv.py
@@ -21,7 +21,13 @@ def make_folds(data_dict,k_folds=10):
     for i,name_i in enumerate(names):
         folds[i % k_folds].append(name_i)
     return CVFolds(data_dict,folds)
-    	
 
-folds=make_folds('wine.json',k_folds=10)
-folds.save('wine_cv')
+def read_folds(in_path):
+    with open(f'{in_path}/folds', 'r') as f:
+        folds = json.load(f)
+        data_dict=data.read_data(f'{in_path}/data')
+        return CVFolds(data_dict,folds)
+
+#folds=make_folds('wine.json',k_folds=10)
+#folds.save('wine_cv')
+read_folds('wine_cv')
