8cba084f9c455d92e03cdd69cf6ce665bcd3279e
tjacek <tjacek@agh.edu.pl>
2023-01-18 23:54:51 +0100

    InlinerVoting classs

--- ECSCF/inliner.py
+++ ECSCF/inliner.py
@@ -1,20 +1,26 @@
 from sklearn import neighbors
 import data,ecscf,learn
 
-def inliner_voting(in_path):
-    ens_i=ecscf.read_binary_ensemble(in_path)
-    votes_i= ens_i.evaluate(True)
-    knn=get_knn(ens_i,k=3)
-    s_clf={}
-    for name_j in votes_i.names():
-        votes_j=votes_i.get_classes(name_j)
-        knn_j=knn.get_classes(name_j)
-        s_clf[name_j]=[t 
-                for t,(v,k) in enumerate(zip(votes_j,knn_j))
-                    if(v==k)]
-    inliner_result=votes_i.dynamic_voting(s_clf) 
-    print(inliner_result.get_acc())
-    print(votes_i.vote().get_acc())
+class InlinerVoting(object):
+    def __init__(self,k=3):
+        self.k=k
+
+
+    def __call__(self,in_path):
+        ens_i=ecscf.read_binary_ensemble(in_path)
+        votes_i= ens_i.evaluate(True)
+        knn=get_knn(ens_i,k=self.k)
+        s_clf={}
+        for name_j in votes_i.names():
+            votes_j=votes_i.get_classes(name_j)
+            knn_j=knn.get_classes(name_j)
+            s_clf[name_j]=[t 
+                    for t,(v,k) in enumerate(zip(votes_j,knn_j))
+                        if(v==k)]
+        inliner_result=votes_i.dynamic_voting(s_clf)
+        return inliner_voting
+#    print(inliner_result.get_acc())
+#    print(votes_i.vote().get_acc())
 
 def get_knn(ens_i,k=3):
     clf= neighbors.KNeighborsClassifier(k)
