c2ccd66b656e7aec83a65c503abc52dc006bc9ed
tjacek <tjacek@agh.edu.pl>
2023-01-21 20:05:43 +0100

    scatter_plot

--- ECSCF/plot.py
+++ ECSCF/plot.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd 
 
 def prepare_data(in_path,stats_path):
@@ -12,4 +14,45 @@ def prepare_data(in_path,stats_path):
     final_df=pd.merge(stats_df,clf_df,on='Dataset',how = 'inner')
     return final_df
 
-prepare_data('uci.csv','stats.csv')
+
+def scatter_plot(df,col='Classes',name='title'):
+    diff= 100*(df['ECSCF']-df['RF']).to_numpy()
+    ind_var=df[col].to_numpy()
+    if(np.amax(ind_var)>10):
+        ind_var=(10/np.amax(ind_var))*ind_var
+    y=df['Dataset']
+
+    print(diff)
+    plt.figure()
+    ax = plt.subplot(111)
+    for i,y_i in enumerate(y):    
+        plt.text(ind_var[i], diff[i], y_i,
+                   fontdict={'weight': 'bold', 'size': 9})
+    
+    plt.ylim(get_limit(diff)) 
+    plt.xlim(get_limit(ind_var))
+    
+    start, stop = ax.get_ylim()
+    ticks = np.arange(int(start)-1, int(stop) + 1, 2)
+    ax.set_yticks(ticks)
+
+    start, stop = ax.get_xlim()
+    ticks = np.arange(int(start)-1, int(stop) + 1, 1)
+    ax.set_xticks(ticks)
+    plt.grid()
+    plt.ylabel("NECSCF-RF accuracy [%]")
+    plt.xlabel(f"{col}")    
+    plt.title(f'NECSCF-RF accuracy as function of {col}')
+    plt.show()
+
+def get_limit(series):
+    s_max=np.amax(series)
+    s_min=np.amin(series)
+    if(s_min>0):
+        s_min=0
+    else:
+        s_min-=1
+    return [s_min,s_max+2]
+
+df=prepare_data('uci.csv','stats.csv')
+scatter_plot(df,col='Samples')
