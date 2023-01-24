428bb3c705b9ff94734a00f48b2a264d243e6c2c
tjacek <tjacek@agh.edu.pl>
2023-01-24 23:37:21 +0100

    BasicExp class

--- ECSCF/output.py
+++ ECSCF/output.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn import ensemble
+from sklearn.linear_model import LogisticRegression
 import protocols,utils
 import inliner
 
@@ -14,24 +15,43 @@ def multi_exp(in_path,exp=None):
             all_lines.append(f'{name_i},{line_j}')
     print('\n'.join(all_lines))
 
-def basic_exp(in_path):
-    lines=['ECSCF']+stats(protocols.escf_exp(in_path))
-    lines=','.join(lines)
-    lines=[lines]
-    algs={'RF':ensemble.RandomForestClassifier(),
-          'Bag':ensemble.BaggingClassifier(),
-          'Grad':ensemble.GradientBoostingClassifier()}
-    for type_i,clf_i in algs.items():
-        print(type_i)
-        acc_i=protocols.check_alg(in_path,clf_i)
-        stats_i=','.join(stats(acc_i))
-        lines.append(f'{type_i},{stats_i}')	
-    print('\n'.join(lines))
-    return lines
+class BasicExp(object):
+    def __init__(self,algs=['LR']):
+        self.algs=get_algs( algs)
+
+    def __call__(self,in_path):
+        lines=['ECSCF']+stats(protocols.escf_exp(in_path))
+        lines=','.join(lines)
+        lines=[lines]
+#    algs={'RF':ensemble.RandomForestClassifier(),
+#          'Bag':ensemble.BaggingClassifier(),
+#          'Grad':ensemble.GradientBoostingClassifier()}
+#    algs={'LR':LogisticRegression}
+        for type_i,clf_i in self.algs.items():
+            print(type_i)
+            acc_i=protocols.check_alg(in_path,clf_i)
+            stats_i=','.join(stats(acc_i))
+            lines.append(f'{type_i},{stats_i}')	
+        print('\n'.join(lines))
+        return lines
 
 def stats(acc):
     return [f'{fun_i(acc):.4f}' 
         for fun_i in [np.mean,np.std]]
+
+def get_algs(names):
+    alg={}
+    for name_i in names:
+        if(name_i=="LR"):
+            alg[name_i]=LogisticRegression()
+        if(name_i=="RF"):
+            alg[name_i]=ensemble.RandomForestClassifier()
+        if(name_i=='Bag'):
+            alg[name_i]=ensemble.BaggingClassifier()
+        if(name_i=='Grad'):
+            alg[name_i]=ensemble.GradientBoostingClassifier()
+
+    return alg
 
 def inliner_exp(in_path):
     inliner_voting=inliner.InlinerVoting()
@@ -42,5 +62,7 @@ def inliner_exp(in_path):
     return [line_i]
 
 #multi_exp('uci')
-multi_exp('uci',inliner_exp)
-#basic_exp('wine_cv2')
+#multi_exp('imp',inliner_exp)
+basic_exp=BasicExp()
+lines=basic_exp('imb/wine-quality-red')
+print(lines)
