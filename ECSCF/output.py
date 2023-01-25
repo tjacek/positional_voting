6e501b4e5011a0800fbc84e16884e05a715bae5b
tjacek <tjacek@agh.edu.pl>
2023-01-25 01:38:46 +0100

    use_escf

--- ECSCF/output.py
+++ ECSCF/output.py
@@ -16,17 +16,14 @@ def multi_exp(in_path,exp=None):
     print('\n'.join(all_lines))
 
 class BasicExp(object):
-    def __init__(self,algs=['LR']):
+    def __init__(self,algs=['LR'],use_escf=True):
         self.algs=get_algs( algs)
+        self.use_escf=use_escf
 
     def __call__(self,in_path):
-        lines=['ECSCF']+stats(protocols.escf_exp(in_path))
-        lines=','.join(lines)
-        lines=[lines]
-#    algs={'RF':ensemble.RandomForestClassifier(),
-#          'Bag':ensemble.BaggingClassifier(),
-#          'Grad':ensemble.GradientBoostingClassifier()}
-#    algs={'LR':LogisticRegression}
+        lines=[]
+        if(self.use_escf):
+            lines.append(self.get_ecscf())
         for type_i,clf_i in self.algs.items():
             print(type_i)
             acc_i=protocols.check_alg(in_path,clf_i)
@@ -34,6 +31,10 @@ class BasicExp(object):
             lines.append(f'{type_i},{stats_i}')	
         print('\n'.join(lines))
         return lines
+
+    def get_ecscf(in_path):
+        lines=['ECSCF']+stats(protocols.escf_exp(in_path))
+        return ','.join(lines) 
 
 def stats(acc):
     return [f'{fun_i(acc):.4f}' 
@@ -43,14 +44,13 @@ def get_algs(names):
     alg={}
     for name_i in names:
         if(name_i=="LR"):
-            alg[name_i]=LogisticRegression()
+            alg[name_i]=LogisticRegression(solver='liblinear')
         if(name_i=="RF"):
             alg[name_i]=ensemble.RandomForestClassifier()
         if(name_i=='Bag'):
             alg[name_i]=ensemble.BaggingClassifier()
         if(name_i=='Grad'):
             alg[name_i]=ensemble.GradientBoostingClassifier()
-
     return alg
 
 def inliner_exp(in_path):
@@ -61,8 +61,8 @@ def inliner_exp(in_path):
     print(line_i)
     return [line_i]
 
-#multi_exp('uci')
-#multi_exp('imp',inliner_exp)
-basic_exp=BasicExp()
-lines=basic_exp('imb/wine-quality-red')
-print(lines)
+
+basic_exp=BasicExp(['LR','RF'],False)
+#lines=basic_exp('imb/wine-quality-red')
+multi_exp('imb',basic_exp)
+#print(lines)
