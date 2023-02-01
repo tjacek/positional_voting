2ff1e9609a4d09bc091d44ac0d5acc5f04a4d264
tjacek <tjacek@agh.edu.pl>
2023-02-01 18:50:12 +0100

    scaling

--- ECSCF/lasso.py
+++ ECSCF/lasso.py
@@ -1,13 +1,23 @@
+from sklearn import linear_model
+from sklearn import preprocessing
+import numpy as np
 import pandas as pd
 
 class ExpStats(object):
     def __init__(self,results,stats):
         self.results=results
-        self.stats=stats
+        self.cols=stats.columns
+        self.X=stats.to_numpy()[:,1:]
+        self.X=preprocessing.scale(self.X)
+
+    def __call__(self):
+        y=self.get_y()
+        coff=self.lasso_coff(y)
+        print(self.cols)
+        print(coff)
 
     def get_y(self,base=('ECSCF','LR'),
     	      diff=('ECSCF','RF')):
-#    	      var='number of classes'):
         base_df=self.results[(self.results['Ens']==base[0]) &
                              (self.results['Clf']==base[1])]
         diff_df=self.results[(self.results['Ens']==diff[0]) &
@@ -15,10 +25,15 @@ class ExpStats(object):
         y=base_df['Acc'].to_numpy()-diff_df['Acc'].to_numpy()
         return y
 
+    def lasso_coff(self,y):
+        clf = linear_model.Lasso(alpha=0.001)
+        clf.fit(self.X,y)
+        return clf.coef_/np.sum(np.abs(clf.coef_))
+
 def prepare(in_path,stats_path):
     result_df=pd.read_csv(in_path)
     stats_df=pd.read_csv(stats_path)
     return ExpStats(result_df,stats_df)
 
 es=prepare('result.txt','stats.csv')
-es.get_y()
+es()
