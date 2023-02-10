5ce3971339b8b23ff87cbcae48f774ad3440218d
tjacek <tjacek@agh.edu.pl>
2023-02-10 04:28:04 +0100

    one vs all

--- ECSCF/ecscf.py
+++ ECSCF/ecscf.py
@@ -4,18 +4,21 @@ from sklearn.linear_model import LogisticRegression
 from itertools import combinations
 import nn,learn,data
 
-class NeuralEnsemble(BaseEstimator,ClassifierMixin):
-    def __init__(self,escf=None,n_hidden=200,n_epochs=200):
-        if(escf is None):
-            escf=OneVsAll()
-        self.escf=escf
+class NeuralEnsemble(BaseEstimator, ClassifierMixin):
+    def __init__(self,n_hidden=200,n_epochs=200):
         self.n_hidden=n_hidden
         self.n_epochs=n_epochs
         self.batch_size=32
+        self.clf_alg=LogisticRegression(solver='liblinear')
 
     def fit(self,X,targets):
-        self.escf.make_extractor(X,targets,
-            self.n_hidden,self.n_epochs,self.batch_size)         
+        self.make_extractor(X,targets)         
+        features= self.get_features(X)
+        for feat_i,extractor_i in zip(features,self.extractors):
+            clf_i=self.clf_alg.fit(feat_i,targets)
+            facade_i=MulticlassFacade(clf_i,extractor_i)
+            self.estimators_.append(facade_i)
+        return self     
 
     def predict(self,X):
         y=[]
@@ -26,23 +29,32 @@ class NeuralEnsemble(BaseEstimator,ClassifierMixin):
         target=np.sum(y,axis=0)
         return np.argmax(target,axis=1)
 
-class OneVsAll(object):
-    def __init__(self):
+class OneVsAll(NeuralEnsemble):
+    def __init__(self,n_hidden=200,n_epochs=200):
+        super(OneVsAll,self).__init__(n_hidden=200,n_epochs=200)
         self.extractors=[]
         self.estimators_=[]
 
-    def make_extractor(self,X,targets,
-        n_hidden,n_epochs,batch_size):
+    def make_extractor(self,X,targets):
         n_cats=max(targets)+1
         for cat_i in range(n_cats):
             y_i=binarize(cat_i,targets)
-            params={'dims':X.shape[1],'n_cats':2}
-            model_i=nn.SimpleNN(n_hidden=self.n_hidden)(params)
+            nn_params={'dims':X.shape[1],'n_cats':2}
+            model_i=nn.SimpleNN(n_hidden=self.n_hidden)(nn_params)
             model_i.fit(X,y_i,
-                epochs=n_epochs,batch_size=batch_size)
+                epochs=self.n_epochs,batch_size=self.batch_size)
             extractor_i= nn.get_extractor(model_i)
             self.extractors.append(extractor_i)
         return self.extractors   
+
+    def get_features(self,X):
+        binary=[]
+        for extractor_i in self.extractors:
+            binary_i=extractor_i.predict(X)
+            concat_i=np.concatenate([X,binary_i],axis=1)
+            binary.append(concat_i)
+        return binary
+
 
 #class ECSCF(BaseEstimator, ClassifierMixin):
 #    def __init__(self,n_hidden=200,n_epochs=200):
@@ -109,13 +121,13 @@ class OneVsAll(object):
 #            binary.append(concat_i)
 #        return binary
 
-class PairEnsemble(ECSCF):
-    def make_extractor(self,X,targets):
-        n_cats=max(targets)+1
-        pairs = list(combinations(range(n_cats),2))
-        for i,j in pairs:
-            y_ij=[ int(cat==i) for cat in targets
-                    if((cat==i) or (cat==j))]
+#class PairEnsemble(ECSCF):
+#    def make_extractor(self,X,targets):
+#        n_cats=max(targets)+1
+#        pairs = list(combinations(range(n_cats),2))
+#        for i,j in pairs:
+#            y_ij=[ int(cat==i) for cat in targets
+#                    if((cat==i) or (cat==j))]
 
 class MulticlassFacade(object):
     def __init__(self,raw_clf,extractor):
